146e6c997d655e6c5824b0899fb0f9d4eb72d42b
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2021-08-05 16:07:08 +0200

    simple Spring Boot setup

---
+++ src/main/java/ch/zuegi/cook/cookapi/CookApiApplication.java
@@ -0,0 +1,13 @@
+package ch.zuegi.cook.cookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zuegi/cook/cookapi/CookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zuegi.cook.cookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
